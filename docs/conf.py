fda17ef7c2d8e83f41d713343e311a64eb1e0073
cosmicoder <asanghi01@gmail.com>
2021-06-25 09:49:19 +0530

    docs update

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ author = 'Aniket Sanghi and Neev Shah'
 master_doc='index'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = '0.3'
 
 
 # -- General configuration ---------------------------------------------------
